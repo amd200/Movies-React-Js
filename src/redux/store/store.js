b012140277b7335c63c65264ac391135a68a6fa3
amd200 <72322072+amd200@users.noreply.github.com>
2024-12-21 15:23:17 +0200

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore } from "redux";
 import { MoviesReducer } from "../reducer/MoviesReducer";
-import { thunk } from "redux-thunk";
+import thunk from "redux-thunk";
 
 export const store = createStore(MoviesReducer, applyMiddleware(thunk));
